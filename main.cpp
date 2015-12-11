8ee0507751ef9800114b55e6ee94eaebe669671e
Matthias Schmidbauer <m_schmidbauer@web.de>
2015-12-11 18:01:12 +0100

    commit to master

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     // This is for testing
     // and sharing with other devices
-    // Laptop commit
+    // Laptop is working right now on the project
     cout << "Hello, World!" << endl;
     return 0;
 }
